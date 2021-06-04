b713e7a1cdae4180e5fec9caef551d7f8fea3164
Max Howell <mxcl@me.com>
2021-06-04 18:49:45 -0400

    wip

--- lib.ts
+++ lib.ts
@@ -24,7 +24,7 @@ function spawn(arg0: string, args: string[]) {
 
 async function xcselect(constraint: string | undefined) {
   const rv = (await xcodes()).filter(([path, v]) =>
-    constraint ? semver.satisfies(constraint, v) : true
+    constraint ? semver.satisfies(v, constraint) : true
   ).sort((a,b) =>
     semver.compare(a[1], b[1])
   ).pop()
