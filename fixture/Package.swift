32be0078a551a6aaa3845588efd09b5d398d1173
Max Howell <mxcl@me.com>
2021-06-05 08:50:36 -0400

    wip

---
+++ fixture/Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let name = "my-fixture"
+
+let package = Package(
+    name: name,
+    products: [
+        .executable(name: name, targets: [name]),
+    ],
+    targets: [
+        .target(name: name),
+    ]
+)
