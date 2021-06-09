68f798b87783f17989a4c7b260ee37a280058e5d
Max Howell <mxcl@me.com>
2021-06-09 09:01:46 -0400

    Fix JSON parse issue properly

--- lib.test.ts
+++ lib.test.ts
@@ -1,5 +1,5 @@
 const { describe, it } = intern.getPlugin('interface.bdd');
-const { destinations } = require('./lib')
+const { destinations, parseJSON } = require('./lib')
 const { assert } = intern.getPlugin('chai');
 
 describe('destinations are parsed', async () => {
@@ -8,3 +8,31 @@ describe('destinations are parsed', async () => {
   assert.isString(out.iOS)
   assert.isString(out.watchOS)
 });
+
+const json =`
+{
+  "project" : {
+    "configurations" : [
+      "Debug",
+      "Release"
+    ],
+    "name" : "CombineCloudKit",
+    "schemes" : [
+      "CombineCloudKit-Package"
+    ],
+    "targets" : [
+      "CombineCloudKit",
+      "CombineCloudKitPackageDescription",
+      "CombineCloudKitPackageTests",
+      "CombineCloudKitTests",
+      "CombineExpectations",
+      "CombineExpectationsPackageDescription"
+    ]
+  }
+}
+build session not created after 15 seconds - still waiting
+`
+
+describe('Bad xcodebuild output JSON is parsed', () => {
+  parseJSON(json)
+})
